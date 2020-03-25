7df22dbe8f4974d8d007dab9030ea869557129ef
Gur <guermoud.@gmail.com>
2020-03-25 11:56:48 +0100

    mynavbar

---
+++ main.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.querySelector('#nav-mobile').style.display = "block";
+    document.querySelector('#nav-desktop').style.display = "none";
+}
+function closeNav() {
+    document.querySelector('#nav-mobile').style.display = "none";
+    document.querySelector('#nav-desktop').style.display = "flex";
+}
